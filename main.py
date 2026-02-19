8a176108a496ead61eb0b5773b63817f53c41884
Shreyansh Jain <shreyanshwork@gmail.com>
2026-02-19 10:34:34 +0530

    Add health check API endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 from models import db_models
 from database.database import engine
@@ -7,6 +7,11 @@ from routers import todos, auth, users, admin
 app = FastAPI()
 
 db_models.Base.metadata.create_all(bind=engine)
+
+@app.get("/healthy", status_code=status.HTTP_200_OK, include_in_schema=False)
+def health_check():
+    return {'message': 'OK'}
+
 
 app.include_router(router=auth.router)
 app.include_router(router=todos.router)
